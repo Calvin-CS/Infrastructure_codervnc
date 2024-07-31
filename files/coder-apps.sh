5b8ee781306cf5f3ed61f31d43564b9b3b29ffdd
Chris Wieringa <cwieri39@calvin.edu>
2024-07-31 11:12:54 -0400

    updated jupyter venv

--- files/coder-apps.sh
+++ files/coder-apps.sh
@@ -5,6 +5,10 @@ curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --p
 
 # JupyerLab
 apt update -y 
-DEBIAN_FRONTEND=noninteractive apt install -y python3-pip
+DEBIAN_FRONTEND=noninteractive apt install -y python3-pip python3-venv
 rm -rf /var/lib/apt/lists/* 
-pip3 install jupyterlab --break-system-packages
+
+python3 -m venv --symlinks /opt/jupyter
+source /opt/jupyter/bin/activate
+pip3 install jupyterlab
+deactivate
